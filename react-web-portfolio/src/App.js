7c15ebf1d573bdcc60f9f13a26b51cd176efcc62
LeoDuong28 <duongtrongnghia287@gmail.com>
2023-06-15 19:55:14 -0700

    new

--- react-web-portfolio/src/App.js
+++ react-web-portfolio/src/App.js
@@ -1,24 +1,46 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import React, { useState, useEffect } from "react";
+
+//import About from "./components/About";
+//import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 {/*
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 */}
+
+import {
+  BrowerRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 export default function App() {
+
+  const [load, updateLoad] =  useState(true);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className="test-gray-499 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      {/*
-      <Projects />
-      <Skills />
-      <Testimonisals />
-      */}
-      <Contact />
-    </main>
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        
+      </div>
+    </Router>
   );
 }
 
