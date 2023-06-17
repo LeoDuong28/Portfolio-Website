58a2f8d17b9e93a408dd07e249e07515cc7ab2c8
LeoDuong28 <duongtrongnghia287@gmail.com>
2023-06-16 19:25:14 -0700

    new

--- react-web-portfolio/src/App.js
+++ react-web-portfolio/src/App.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from "react";
-
-//import About from "./components/About";
-//import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-{/*
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
-*/}
-
+import Preloader from "./components/PreLoader";
 import {
-  BrowerRouter as Router,
+  BrowserRouter as Router,
   Route,
   Routes,
   Navigate
